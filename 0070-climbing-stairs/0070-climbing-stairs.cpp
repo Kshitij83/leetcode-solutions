6908f3cf2a4fd4f7d5831c3c1f6e75dd749682aa
Kshitij Choudhary <ksh03082004@gmail.com>
2025-06-26 14:04:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (35.98%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findStairs(int n, vector<int>& dp) {
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = findStairs(n-1,dp) + findStairs(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return findStairs(n,dp);
+    }
+};
