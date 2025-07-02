3442598081e73c4ffbdbe8cb9173062a35d6d521
Kshitij Choudhary <ksh03082004@gmail.com>
2025-07-02 15:50:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (83.83%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void dfs(int node, vector<int>& vis, vector<vector<int>>& isConnected) {
+    void dfs(int node, vector<int> &vis, vector<vector<int>>& isConnected) {
         vis[node] = 1;
         for(int i=0;i<isConnected.size();i++) {
-            if(isConnected[node][i]==1 && !vis[i]) dfs(i,vis,isConnected);
+            if(!vis[i] && isConnected[node][i]==1) dfs(i,vis,isConnected);
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> vis(n,0);
+        // TC - O(N) + O(V+2E)
+        int v = isConnected.size();
+        vector<int> vis(v,0);
         int count = 0;
-        for(int i=0;i<n;i++) {
+        for(int i=0;i<v;i++) {
             if(!vis[i]) {
-                count++;
-                cout<<i<<endl;
                 dfs(i,vis,isConnected);
+                count++;
             }
         }
         return count;
