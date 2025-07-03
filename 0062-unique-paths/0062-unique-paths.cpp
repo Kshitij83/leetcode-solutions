47af5dabd5bc3da92b1cbd887b40dad31311ec4a
Kshitij Choudhary <ksh03082004@gmail.com>
2025-07-03 16:41:21 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
-    int uniquePath(int i, int j, vector<vector<int>>& dp) {
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left = uniquePath(i,j-1,dp);
-        int up = uniquePath(i-1,j,dp);
-        return dp[i][j] = left + up;
-    }
     int uniquePaths(int m, int n) {
-        //dp approach, tc - O(n*m) , sc - O(n-1 + m-1) (Path length: stack space) + O(n*m) 
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return uniquePath(m-1,n-1,dp);
-
-
-        //TC - O(2^M*N) , SC - O(PATH LENGTH) SPACE COMPLEXITY CONSISTS OF THE RECURSIVE CALL STACK
-        // if(m==1 && n==1) return 1;
-        // if(m==0 || n==0) return 0;
-        // int left = uniquePaths(m,n-1);
-        // int up = uniquePaths(m-1,n);
-        // return left + up;
+        vector<vector<int>> dp(m,vector<int>(n,1));
+        for(int i=1;i<m;i++) {
+            for(int j=1;j<n;j++) dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+        return dp[m-1][n-1];
     }
 };
