058ade6e78b716dd1354446c48d1c2a05506434a
Kshitij Choudhary <ksh03082004@gmail.com>
2025-07-03 17:10:37 +0530

    Time: 0 ms (100%), Space: 11.8 MB (72.63%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        //tabulation approach : TC - O(n*m) , SC - O(n*m)
         int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size(); 
+        int n = obstacleGrid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,0));
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(obstacleGrid[i][j]==1) dp[i][j] = 0;
-                else if(i==0 && j==0) dp[i][j] = 1;
-                else{
-                    int up = 0;
-                    int left = 0;
-                    if(j>0) left = dp[i][j-1];
-                    if(i>0) up = dp[i-1][j];
-                    dp[i][j] = left + up;
-                }
-            }
+        for(int i=0;i<n;i++) {
+            if(obstacleGrid[0][i]!=1) dp[0][i] = 1;
+            else break;
         }
+        for(int i=0;i<m;i++) {
+            if(obstacleGrid[i][0]!=1) dp[i][0] = 1;
+            else break;
+        }
+        for(int i=1;i<m;i++) for(int j=1;j<n;j++) if(obstacleGrid[i][j]!=1) dp[i][j] = dp[i-1][j] + dp[i][j-1];
         return dp[m-1][n-1];
     }
 };
