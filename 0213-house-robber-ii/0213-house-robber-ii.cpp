44f8d523c05049370b4e19cb1085506993dadc70
Kshitij Choudhary <ksh03082004@gmail.com>
2025-08-05 19:54:46 +0530

    Time: 0 ms (100%), Space: 10.9 MB (20.77%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        return max(helper(vector<int>(nums.begin(),nums.end()-1)),helper(vector<int>(nums.begin()+1,nums.end())));
+    }
+    int helper(const vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[1],nums[0]);
+        for(int i=2;i<n;i++) {
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
+    }
+};
