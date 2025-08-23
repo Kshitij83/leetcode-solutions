0edb2c175c5555efba21871cb44e52d48a6c3ca6
Kshitij Choudhary <ksh03082004@gmail.com>
2025-08-23 19:32:28 +0530

    Time: 4 ms (34.12%), Space: 19.5 MB (38.53%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,16 +5,18 @@ public:
         vector<vector<int>> dp(n+1,vector<int>(2,0));
         dp[n][0] = dp[n][1] = 0;
         for(int i=n-1;i>=0;i--) {
-            for(int j=0;j<=1;j++) {
-                int profit = 0;
-                if(j==1) {
-                    profit = max(-prices[i]+dp[i+1][0],dp[i+1][1]);
-                }
-                else {
-                    profit = max(prices[i]+dp[i+1][1],dp[i+1][0]);
-                }
-                dp[i][j] = profit;
-            }
+            dp[i][0] = max(prices[i]+dp[i+1][1],dp[i+1][0]);
+            dp[i][1] = max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+            // for(int j=0;j<=1;j++) {
+            //     int profit = 0;
+            //     if(j==1) {
+            //         profit = max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+            //     }
+            //     else {
+            //         profit = max(prices[i]+dp[i+1][1],dp[i+1][0]);
+            //     }
+            //     dp[i][j] = profit;
+            // }
         }
         return dp[0][1];
     }
