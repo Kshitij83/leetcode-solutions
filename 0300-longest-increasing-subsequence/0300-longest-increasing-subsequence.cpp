8f093aeef1c7353c3cc146252c29e1354c16b84f
Kshitij Choudhary <ksh03082004@gmail.com>
2025-08-25 19:58:07 +0530

    Time: 148 ms (44.32%), Space: 14.4 MB (52.03%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -10,19 +10,34 @@ public:
     //     return dp[ind][prev_ind+1] = len;
     // }
     int lengthOfLIS(vector<int>& nums) {
-        //Approach 2 : Tabulation
+        //Approach 3 : Tabulation with SC - O(2*N)
         int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        vector<int> prev(n+1,0),curr(n+1,0);
         for(int ind=n-1;ind>=0;ind--) {
             for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--) {
-                int len = dp[ind+1][prev_ind+1];
+                int len = prev[prev_ind+1];
                 if(prev_ind==-1 || nums[ind]>nums[prev_ind]) {
-                    len = max(len,1 + dp[ind+1][ind+1]);
+                    len = max(len,1 + prev[ind+1]);
                 }
-                dp[ind][prev_ind+1] = len;
+                curr[prev_ind+1] = len;
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return prev[0];
+
+        //Approach 2 : Tabulation
+        // int n = nums.size();
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        // for(int ind=n-1;ind>=0;ind--) {
+        //     for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--) {
+        //         int len = dp[ind+1][prev_ind+1];
+        //         if(prev_ind==-1 || nums[ind]>nums[prev_ind]) {
+        //             len = max(len,1 + dp[ind+1][ind+1]);
+        //         }
+        //         dp[ind][prev_ind+1] = len;
+        //     }
+        // }
+        // return dp[0][0];
 
         //Approach 1 : Memoization 
         // int n = nums.size();
