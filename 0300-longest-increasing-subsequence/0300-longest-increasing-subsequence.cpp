8bea73fa60d75ca349fca81ec73f02879f131c39
Kshitij Choudhary <ksh03082004@gmail.com>
2025-08-25 20:03:34 +0530

    Time: 71 ms (58.73%), Space: 14.3 MB (73.81%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -10,20 +10,35 @@ public:
     //     return dp[ind][prev_ind+1] = len;
     // }
     int lengthOfLIS(vector<int>& nums) {
-        //Approach 3 : Tabulation with SC - O(2*N)
+        //Approach 4: Tabulation but used for printing the subsequence too
         int n = nums.size();
-        vector<int> prev(n+1,0),curr(n+1,0);
-        for(int ind=n-1;ind>=0;ind--) {
-            for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--) {
-                int len = prev[prev_ind+1];
-                if(prev_ind==-1 || nums[ind]>nums[prev_ind]) {
-                    len = max(len,1 + prev[ind+1]);
+        vector<int> dp(n,1);
+        int maxi = 1;
+        for(int i=1;i<n;i++) {
+            for(int j=0;j<i;j++) {
+                if(nums[j]<nums[i]) {
+                    dp[i] = max(dp[i],1+dp[j]);
+                    maxi = max(dp[i],maxi); 
                 }
-                curr[prev_ind+1] = len;
             }
-            prev = curr;
         }
-        return prev[0];
+        return maxi;
+
+
+        //Approach 3 : Tabulation with SC - O(2*N)
+        // int n = nums.size();
+        // vector<int> prev(n+1,0),curr(n+1,0);
+        // for(int ind=n-1;ind>=0;ind--) {
+        //     for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--) {
+        //         int len = prev[prev_ind+1];
+        //         if(prev_ind==-1 || nums[ind]>nums[prev_ind]) {
+        //             len = max(len,1 + prev[ind+1]);
+        //         }
+        //         curr[prev_ind+1] = len;
+        //     }
+        //     prev = curr;
+        // }
+        // return prev[0];
 
         //Approach 2 : Tabulation
         // int n = nums.size();
