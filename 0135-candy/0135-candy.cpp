b5f9e586447435f8c3490b425a5252e4a264bdc0
Kshitij Choudhary <ksh03082004@gmail.com>
2025-09-09 19:22:13 +0530

    Time: 1 ms (35.92%), Space: 21.9 MB (95.56%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+        //TC - O(N) , SC - O(1) , Slope method
         int n = ratings.size();
-        vector<int> left(n,1);
-        for(int i=1;i<n;i++) {
-            if(ratings[i]>ratings[i-1]) left[i] = left[i-1] + 1;
-            else left[i] = 1;
+        int i = 1;
+        int sum = 1;
+        while(i<n) {
+            if(ratings[i]==ratings[i-1]) {
+                sum++;
+                i++;
+                continue;
+            }
+            int peak = 1;
+            while(i<n && ratings[i]>ratings[i-1]) {
+                peak++;
+                sum += peak;
+                i++;
+            }
+            int down = 1;
+            while(i<n && ratings[i]<ratings[i-1]) {
+                sum += down;
+                down++;
+                i++;
+            }
+            if(down>peak) sum += down-peak;
         }
-        int total = left[n-1];
-        int right = left[n-1];
-        for(int i=n-2;i>=0;i--) {
-            if(ratings[i]>ratings[i+1]) right++;
-            else right = 1;
-            total += max(left[i],right);
-        }
-        return total;
+        return sum;
+
+        //TC - O(2N) , SC - O(N)
+        // int n = ratings.size();
+        // vector<int> left(n,1);
+        // for(int i=1;i<n;i++) {
+        //     if(ratings[i]>ratings[i-1]) left[i] = left[i-1] + 1;
+        //     else left[i] = 1;
+        // }
+        // int total = left[n-1];
+        // int right = left[n-1];
+        // for(int i=n-2;i>=0;i--) {
+        //     if(ratings[i]>ratings[i+1]) right++;
+        //     else right = 1;
+        //     total += max(left[i],right);
+        // }
+        // return total;
     }
 };
