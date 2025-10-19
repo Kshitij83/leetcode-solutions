9464ee5d76ad36e61485ce658a52e5fd4b0754dd
Kshitij Choudhary <ksh03082004@gmail.com>
2025-10-19 14:05:35 +0530

    Time: 8 ms (74.41%), Space: 19.2 MB (78.64%) - LeetHub

--- 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.cpp
@@ -1,47 +1,14 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-        int res = 0;
-        stack<int> st;
-        int count1 = 1;
-        int count2 = 2;
-        int n = directions.length();
-        for(int i = 0; i < n; i++)
-        {
-            if(st.empty())
-            {
-                st.push(directions[i]);
-            }
-            else
-            {
-                if(!st.empty() && st.top() == 'R' && directions[i] == 'L')
-                {
-                    res+=count2;
-                    st.pop();
-                    st.push('S');
-                    st.push('S');
-                }
-                else if(!st.empty() && st.top() == 'R' && directions[i] == 'S' || !st.empty() && st.top() == 'S' && directions[i] == 'L')
-                {
-                    res += count1;
-                    st.pop();
-                    st.push('S');
-                    st.push('S');
-                }
-                else
-                {
-                    st.push(directions[i]);
-                }
-
-                // not handeling the cases when top and directions are same letters
-            }
+        int left = 0;
+        int right = directions.size() - 1;
+        while(left<directions.size() && directions[left]=='L') left++;
+        while(right>=0 && directions[right]=='R') right--;
+        int count = 0;
+        for(int i=left;i<=right;i++) {
+            if(directions[i]!='S') count++;
         }
-        int foundS = 0;
-        while(!st.empty()) {
-            if(st.top()=='S') foundS = 1;
-            if(st.top()=='R' && foundS) res++;
-            st.pop();
-        }
-        return res;
+        return count;
     }
 };
